e992880c2137ffdd772ff5170d0c8056c548055b
Paweł Słowik <pawel.slowik@schibsted.pl>
2017-01-31 11:14:49 +0100

    bootstrap support

---
+++ src/Layout.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router'
+
+class Layout extends Component {
+    render() {
+        return (
+            <div>
+                <div className="page-header">
+                    <h1>Moje miony</h1>
+                </div>
+                <ul>
+                    <li><Link to="/users">users</Link></li>
+                    <li><Link to="/about">about</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+export default Layout
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+import Layout from './Layout.js'
 
-const Users = React.createClass({
+class Groups extends Component {
 
     render() {
         return (
@@ -13,7 +14,7 @@ const Users = React.createClass({
                     <ul>
                         {/* use Link to route around the app */}
                         {this.state.users.map(user => (
-                            <li key={user.id}><Link to={`/user/${user.id}`}>{user.name}</Link></li>
+                            <li key={user.id}><Link to={`/users/${user.id}`}>{user.name}</Link></li>
                         ))}
                     </ul>
                 </div>
@@ -22,42 +23,44 @@ const Users = React.createClass({
                 </div>
             </div>
         )
-    },
+    }
+
     componentWillMount() {
         this.setState({
             users: [{id: 1, name: "dupa"}]
         })
     }
-});
+}
 
-const About = React.createClass({
+class About extends Component {
     render() {
         return (
             <div>About</div>
         )
     }
-});
+}
 
-const NoMatch = React.createClass({
+class NoMatch extends Component {
     render() {
         return (
             <div>NoMatch</div>
         )
     }
-});
+}
 
-const User = React.createClass({
+class Group extends Component {
 
     componentWillMount() {
         this.setState({
             // route components are rendered with useful information, like URL params
             user: this.findUserById(this.props.params.userId)
         })
-    },
+    }
 
-    findUserById(id){
+    findUserById(id) {
         return {id: 1, name: "dfas"}
-    },
+    }
+
     render() {
         return (
             <div>
@@ -66,32 +69,17 @@ const User = React.createClass({
             </div>
         )
     }
-});
-
-const App = React.createClass({
-    render() {
-        return (
-            <div>
-                <div>App</div>
-                <ul>
-                    <li><Link to="/users">users</Link></li>
-                    <li><Link to="/about">about</Link></li>
-                </ul>
-                {this.props.children}
-            </div>
-        )
-    }
-});
+}
 
 class Routes extends Component {
     render() {
         return (
             <Router history={browserHistory}>
-                <Route path="/" component={App}>
-                    <IndexRoute component={Users}/>
+                <Route path="/" component={Layout}>
+                    <IndexRoute component={Groups}/>
                     <Route path="about" component={About}/>
-                    <Route path="users" component={Users}>
-                        <Route path="/user/:userId" component={User}/>
+                    <Route path="users" component={Groups}>
+                        <Route path="/users/:userId" component={Group}/>
                     </Route>
                     <Route path="*" component={NoMatch}/>
                 </Route>
